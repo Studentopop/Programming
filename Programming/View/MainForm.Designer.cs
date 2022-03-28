52b935e74101ffe97be9ea15bb0208cb1409200b
Studentopop <99469118+Studentopop@users.noreply.github.com>
2022-03-28 22:35:37 +0700

    class1

--- Programming/View/MainForm.Designer.cs
+++ Programming/View/MainForm.Designer.cs
@@ -48,6 +48,7 @@ namespace Programming.View
             this.EnumsListBox = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.TabEnums = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.EnumPage.SuspendLayout();
             this.SeasonHandleGroupBox.SuspendLayout();
             this.WeekdayParsingGroupBox.SuspendLayout();
@@ -249,6 +250,7 @@ namespace Programming.View
             // TabEnums
             // 
             this.TabEnums.Controls.Add(this.EnumPage);
+            this.TabEnums.Controls.Add(this.tabPage1);
             this.TabEnums.Dock = System.Windows.Forms.DockStyle.Fill;
             this.TabEnums.Location = new System.Drawing.Point(0, 0);
             this.TabEnums.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -256,6 +258,16 @@ namespace Programming.View
             this.TabEnums.SelectedIndex = 0;
             this.TabEnums.Size = new System.Drawing.Size(848, 515);
             this.TabEnums.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(840, 482);
+            this.tabPage1.TabIndex = 1;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -301,5 +313,6 @@ namespace Programming.View
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button GoButton;
         private System.Windows.Forms.ComboBox ChooseSeasonBox;
+        private System.Windows.Forms.TabPage tabPage1;
     }
 }
