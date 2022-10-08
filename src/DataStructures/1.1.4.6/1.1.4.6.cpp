7d85f194ed010fef906d978872ab2aacf0b7df64
Studentopop <99469118+Studentopop@users.noreply.github.com>
2022-10-08 10:20:18 +0700

    1.1.4.6

---
+++ src/DataStructures/1.1.4.6/1.1.4.6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 5;
+	int* pointer = &a;
+	cout << "Address of a: " << &a << endl;
+	cout << "Address in pointer: " << pointer << endl;
+	cout << "Address of pointer: " << &pointer << endl;
+	cout << endl;
+	*pointer = 7;
+	cout << "Value in a: " << a << endl;
+	cout << "Value by pointer address: " << *pointer << endl;
+}
