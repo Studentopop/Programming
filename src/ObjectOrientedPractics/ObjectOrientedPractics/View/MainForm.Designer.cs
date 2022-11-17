852080fb970159f8381b5eedf086f9eac80d7a72
Studentopop <99469118+Studentopop@users.noreply.github.com>
2022-11-17 22:23:11 +0700

    Исправил

--- src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -47,7 +47,7 @@ namespace ObjectOrientedPractics.View
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(906, 561);
+            this.tabControl1.Size = new System.Drawing.Size(920, 565);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -56,7 +56,7 @@ namespace ObjectOrientedPractics.View
             this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(898, 528);
+            this.tabPage1.Size = new System.Drawing.Size(912, 532);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Items";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -66,7 +66,7 @@ namespace ObjectOrientedPractics.View
             this.itemsTab1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.itemsTab1.Location = new System.Drawing.Point(3, 3);
             this.itemsTab1.Name = "itemsTab1";
-            this.itemsTab1.Size = new System.Drawing.Size(892, 522);
+            this.itemsTab1.Size = new System.Drawing.Size(906, 526);
             this.itemsTab1.TabIndex = 0;
             // 
             // tabPage2
@@ -92,7 +92,7 @@ namespace ObjectOrientedPractics.View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(906, 561);
+            this.ClientSize = new System.Drawing.Size(920, 565);
             this.Controls.Add(this.tabControl1);
             this.MinimumSize = new System.Drawing.Size(924, 608);
             this.Name = "MainForm";
