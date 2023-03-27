b63393c95bc9e2156b6c4012df644bb722c7c128
Studentopop <99469118+Studentopop@users.noreply.github.com>
2023-03-27 12:17:15 +0700

    Сделал, осталось проверить всё и понять тему получше. Имею в виду добавить комментарии, перепроверить

---
+++ src/Contacts/Contacts/Contacts/Model/Contact.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Model
+{
+    /// <summary>
+    /// Хранит данные о контакте.
+    /// </summary>
+    public class Contact
+    {
+        /// <summary>
+        /// Возвращает и задает имя контакта.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Возвращает и задает номер телефона контакта.
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Возвращает и задает почту контакта.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        public Contact()
+        {
+        }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="name">Имя.</param>
+        /// <param name="phoneNumber">Номер телефона.</param>
+        /// <param name="email">Почта.</param>
+        [JsonConstructor]
+        public Contact(string name, string phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
