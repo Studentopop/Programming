ca50e7c82667128f5dd00d20a116113b82400c33
Studentopop <99469118+Studentopop@users.noreply.github.com>
2023-06-20 19:26:07 +0700

    5

---
+++ src/Contacts/Contacts/Model/Contact.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace View.Model
+{
+    public class Contact : ICloneable
+    {
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        /// <param name="name">Имя.</param>
+        /// <param name="email">Электронная почта.</param>
+        /// <param name="phone">Номер телефона.</param>
+        public Contact(string name, string email, string phone)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+
+        /// <summary>
+        /// Создаёт экземпляр класса <see cref="Contact"/>.
+        /// </summary>
+        public Contact()
+        {
+
+        }
+
+        /// <summary>
+        /// Возвращает и задаёт имя контакта.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Возвращает и задаёт электронную почту контакта.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Возвращает и задаёт номер телефона контакта.
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Возвращает копию экземпляра класса <see cref="Contact"/>
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            return new Contact(Name, Email, Phone);
+        }
+
+
+    }
+}
